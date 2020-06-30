38953d0dc1c38a55aaa7cd4aeab3dba3a72a4186
Arthur Chang <07570.arthur.chang@ICT888.NET>
2020-06-30 15:46:52 +0800

    CheckIn.

--- CAS.UT.Practice.Libs.Unit.Tests/TennisGameTests.cs
+++ CAS.UT.Practice.Libs.Unit.Tests/TennisGameTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace CAS.UT.Practice.Libs.Unit.Tests
 {
@@ -6,8 +7,33 @@ namespace CAS.UT.Practice.Libs.Unit.Tests
     public class TennisGameTests
     {
         [Test]
-        public void Test()
+        public void Should_DisplayNameIsGame20200703_When_NameIsGame_And_OccuredDateIsDefaultValue()
         {
+            //TODO: Arrange
+            var name = "Game";
+            var occuredDate = new DateTimeOffset(2020, 07, 03, 0, 0, 0, TimeSpan.Zero);
+            var game = new TennisGame(name, occuredDate);
+            var expected = "Game20200703";
+
+            //TODO: Action
+            var actual = game.DisplayName;
+
+            //TODO: Assertion
+            Assert.AreEqual(expected, actual);
+        }
+
+        //Another Test Case: Name is "G2ame" And occuredDate is 1937/07/07
+        [Test]
+        public void Should_DisplayNameIsG2ame19370707_When_NameIsG2ame_And_OccuredDateIs19370707()
+        {
+            var name = "Game";
+            var occuredDate = new DateTimeOffset(2020, 07, 03, 0, 0, 0, TimeSpan.Zero);
+            var game = new TennisGame(name, occuredDate);
+            var expected = "Game20200703";
+
+            var actual = game.DisplayName;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- CAS.UT.Practice.Libs/TennisGame.cs
+++ CAS.UT.Practice.Libs/TennisGame.cs
@@ -1,6 +1,35 @@
-﻿namespace CAS.UT.Practice.Libs
+﻿using System;
+
+namespace CAS.UT.Practice.Libs
 {
     public class TennisGame
     {
+        #region Fields
+
+        private readonly string _name;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public TennisGame()
+        {
+        }
+
+        public TennisGame(string name, DateTimeOffset? occuredDate = null)
+        {
+            _name = name;
+            OccuredDate = occuredDate ?? DateTimeOffset.Now;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public string DisplayName => $"{_name}{OccuredDate:yyyyMMdd}";
+
+        public DateTimeOffset OccuredDate { get; } = DateTimeOffset.Now;
+
+        #endregion Properties
     }
 }
