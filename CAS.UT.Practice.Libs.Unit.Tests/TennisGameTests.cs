f9eac51aff3d45c65dc64bea6661e1378f67a7af
Arthur Chang <07570.arthur.chang@ICT888.NET>
2020-06-30 16:22:18 +0800

    CheckIn.

--- CAS.UT.Practice.Libs.Unit.Tests/TennisGameTests.cs
+++ CAS.UT.Practice.Libs.Unit.Tests/TennisGameTests.cs
@@ -6,8 +6,19 @@ namespace CAS.UT.Practice.Libs.Unit.Tests
     public class TennisGameTests
     {
         [Test]
-        public void Test()
+        public void Should_IsEndIsTrue_When_ForceTerminateIsCalled()
         {
+            //TODO: Arrange
+
+            //TODO: Action
+
+            //TODO: Assertion
         }
+
+        //Case: If IsEnd is true, client call ForceTerminate method would not change anything.
+
+        //Case: If IsEnd is true, client call ForceTerminate method would throw an Exception: IllegalOperationException.
+
+        //Case: When occuredDate is future time, client should not call ForceTerminate.
     }
 }
